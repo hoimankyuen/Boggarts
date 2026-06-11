bed51b29ee2ca9c42f39e45bf7428ddf4c063f24
Paul Gush <Paul1208Gush@Gmail.com>
2026-06-11 14:22:42 +0100

    Gate/Choice data structure

---
+++ Boggarts_Unity/Assets/Scripts/Boggarts/Data/Choice.cs
@@ -0,0 +1,10 @@
+using UnityEditor;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Boggart Data/Choice")]
+public class Choice : ScriptableObject
+{
+    [Tooltip("Denotes a good choice or a bad choice")] public bool IsGood;
+    [Tooltip("Sound to play on selecting the choice")] public AudioClip Sound;
+    [Tooltip("Scene to load based on choice")] public SceneAsset Scene;
+}
---
+++ Boggarts_Unity/Assets/Scripts/Boggarts/Data/Gate.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Boggart Data/Gate")]
+public class Gate : ScriptableObject
+{
+    public Choice[] Choices;
+}
